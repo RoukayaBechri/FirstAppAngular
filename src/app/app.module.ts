64472a359293279ea7acb865f85a4b40e4dcec56
RoukayaBechri <bechri.roukaya@gmail.com>
2018-11-16 12:43:05 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputTestComponent } from './input-test/input-test.component';
+import { MenuComponent } from './menu/menu.component';
+import { BodyComponent } from './body/body.component';
+import { SliderComponent } from './slider/slider.component';
+import { FooterComponent } from './footer/footer.component';
+import { CubeComponent } from './cube/cube.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputTestComponent,
+    MenuComponent,
+    BodyComponent,
+    SliderComponent,
+    FooterComponent,
+    CubeComponent,
+    
   ],
   imports: [
     BrowserModule,
